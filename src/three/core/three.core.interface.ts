8c6c16a08450ab2da0ac8b7320264c30d25a1ff9
Defkil <Defkil@users.noreply.github.com>
2023-03-06 18:44:35 +0000

    deploy: 79f48e669bc1ffbd3b7906b6030ebbce03450f58

--- src/three/core/three.core.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Three core with animation loop
- */
-export interface ThreeCoreInterface {
-  /** Setup three core */
-  setup: () => Promise<void>
-  /** Start three core and loop */
-  start: () => Promise<void>
-}
